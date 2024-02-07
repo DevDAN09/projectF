373a703a2a6c7fbc991b904b6d97c72420da09ca
devDAN09 <devyun09@gmail.com>
2024-02-07 16:53:01 +0900

    Draggable update

--- lib/core/common/view/test_paper.dart
+++ lib/core/common/view/test_paper.dart
@@ -4,15 +4,25 @@ import 'package:projectf/core/common/view/name_tag.dart';
 
 class TestPaper extends StatefulWidget {
   final String opinion;
+  final String name;
   final double height;
   final double width;
+  final double angle;
+  final Offset offset;
   final double fontSize;
+  final bool isSelected;
+  final Function onNameAccepted;
 
-  const TestPaper({
+  TestPaper({
     required this.opinion,
+    required this.onNameAccepted,
+    this.name = "",
     this.height = 200,
     this.width = 150,
     this.fontSize = 45,
+    this.isSelected = false,
+    this.offset = const Offset(0,0),
+    this.angle = 0.0,
     super.key
   });
 
@@ -21,53 +31,82 @@ class TestPaper extends StatefulWidget {
 }
 
 class _TestPaperState extends State<TestPaper> {
-  Image imageData = Image.asset('assets/images/nameTag.png', fit: BoxFit.contain);
+  late String name;
+  late Widget imageData;
+
+  @override
+  void initState() {
+    super.initState();
+    imageData = NameTag(
+        name: widget.name,
+        height: widget.height,
+        width: widget.width,
+        fontSize: widget.fontSize
+    );
+  }
+
+  void updateName(String newName) {
+    setState(() {
+      imageData = NameTag(
+          name: newName,
+          height: widget.height,
+          width: widget.width,
+          fontSize: 25
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: widget.height,
-        width: widget.width,
-        margin: EdgeInsets.all(12),
-        child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                  child: Container(
-                    height: widget.height,
-                    width: widget.width,
-                    color: ColorSystem.white,
+      child: Transform.rotate(
+        angle: widget.angle,
+        child: Transform.translate(
+          offset: widget.offset,
+          child: DragTarget(
+            onAccept: (String data) {
+              updateName(data);
+              widget.onNameAccepted(data);
+              print("draged");
+            },
+            builder: (BuildContext context, List<dynamic> accepted, List<dynamic> rejected) {
+              return Container(
+                  height: widget.height,
+                  width: widget.width,
+                  margin: EdgeInsets.all(12),
+                  child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Positioned(
+                            child: Container(
+                              height: widget.height,
+                              width: widget.width,
+                              color: ColorSystem.white,
+                            )
+                        ),
+                        Positioned(
+                            child: Transform.translate(
+                                offset: Offset(0, -50),
+                                child: Container(
+                                  margin: EdgeInsets.only(left:10, right:10),
+                                  child: imageData,
+                                )
+                            )
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20, left: 10, right: 10),
+                          child: Text(
+                            widget.opinion,
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ]
                   )
-              ),
-              Positioned(
-                  child: Transform.translate(
-                      offset: Offset(0, -50),
-                      child: DragTarget(
-                        builder: (
-                            BuildContext context,
-                            List<dynamic> accepted,
-                            List<dynamic> rejected,
-                            ) {
-                          return Container(
-                            margin: EdgeInsets.only(left:10, right:10),
-                            child: imageData,
-                          );
-                        },
-                        onAccept: (String data) {
-                          setState(() {
-                            imageData = NameTag(name: data) as Image;
-                          });
-                        },
-                      )
-                  )
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 20, left: 10, right: 10),
-                child: Text(widget.opinion,
-                  textAlign: TextAlign.center,),
-              )
-            ]
-        )
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
